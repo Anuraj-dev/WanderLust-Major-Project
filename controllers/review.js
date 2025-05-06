d1b6bd29f7b3d9c003e792a93dc48ef375b00ae2
Anuraj Jit Saikia <anurajjit@gmail.com>
2025-05-06 16:37:10 +0530

    fix: directory now lowercase

---
+++ controllers/review.js
@@ -0,0 +1,31 @@
+const Listing = require("../Models/listing");
+const Review = require("../Models/review");
+
+module.exports.createReview = async (req, res) => {
+  let { id } = req.params;
+  const reviewData = req.body.review;
+
+  let listing = await Listing.findById(id);
+  let newReview = new Review(reviewData);
+  newReview.author = req.user._id;
+
+  listing.reviews.push(newReview);
+
+  await newReview.save();
+  await listing.save();
+
+  req.flash("success", "New Review Created!");
+
+  res.redirect(`/listings/${id}`);
+};
+
+module.exports.destroyReview = async (req, res) => {
+  let { id, review_id } = req.params;
+  //Delete ref from listings
+  await Listing.findByIdAndUpdate(id, { $pull: { reviews: review_id } });
+  //Delete the review itself
+  await Review.findByIdAndDelete(review_id);
+
+  req.flash("success", "Review Deleted!");
+  res.redirect(`/listings/${id}`);
+};
---
+++ models/listing.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const { listingSchema } = require("../schema");
+const Schema = mongoose.Schema;
+const Reviews = require("./review");
+
+//
+
+const listingsSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    url: String,
+    filename: String,
+  },
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  geometry: {
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ["Point"], // 'location.type' must be 'Point'
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
+  category: {
+    type: String,
+    enum: [
+      "Trending",
+      "Rooms",
+      "Top Cities",
+      "Mountains",
+      "Castles",
+      "Pools",
+      "Camping",
+      "Farms",
+      "Arctic",
+      "Beach",
+    ],
+  },
+});
+
+listingsSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Reviews.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
+const Listing = mongoose.model("Listing", listingsSchema);
+module.exports = Listing;
